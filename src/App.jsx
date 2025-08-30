98dffca964ffe3f9130d8cca8a1e2ff4c8ead853
pularu9211 <pularupoornima9211@gmail.com>
2025-08-31 02:59:40 +0530

    project added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./components/hero";
+import Navigation from "./components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
